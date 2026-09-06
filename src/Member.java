44202641a87b986a1fda97c2f8e189cfd941291b
abianHakim <abianhakim09@gmail.com>
2026-09-06 22:08:16 +0700

    feat: implementasi relasi one-to-many Member-Buku

--- src/Member.java
+++ src/Member.java
@@ -19,4 +19,33 @@ public class Member {
     public String getIdMember(){
         return idMember;
     }
+
+    public void pinjamBuku(Buku b){
+        if(b.isTersedia()){
+            daftarPinjaman.add(b);
+            b.setPeminjam(this);
+            System.out.println(nama + " berhasil meminjam \"" + b.getJudul() + "\"");
+        } else {
+            System.out.println("Gagal: \"" + b.getJudul() + "\" sedang dipinjam oleh " + b.getPeminjam().getNama());
+        }
+    }
+
+    public void kembalikanBuku(Buku b){
+        if(daftarPinjaman.contains(b)){
+            daftarPinjaman.remove(b);
+            b.setPeminjam(null);
+            System.out.println(nama + " mengembalikan \"" + b.getJudul() + "\"");
+        } else {
+            System.out.println(nama + " tidak sedang meminjam \"" + b.getJudul() + "\"");
+        }
+    }
+
+    public void tampilkanPinjaman(){
+        System.out.println("Daftar Pinjaman " + nama + ":");
+        int i = 1;
+        for(Buku b : daftarPinjaman){
+            System.out.println(i + ". " + b.getKodeBuku() + " - " + b.getJudul());
+            i++;
+        }
+    }
 }
