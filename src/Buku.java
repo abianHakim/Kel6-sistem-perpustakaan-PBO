515e457ad040495038cfc00ffa1e6941d582b8e7
dederismakomalasari-cmd <dederismakomalasari@gmail.com>
2026-09-06 20:46:54 +0700

    update buku

--- src/Buku.java
+++ src/Buku.java
@@ -1,3 +1,39 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Buku {
-    
+    private String judul;
+    private String kodeBuku;
+    private List<GenreBuku> daftarGenre; // many-to-many ke GenreBuku
+    private Member peminjam;             // many-to-one ke Member, null = tersedia
+
+    public Buku(String judul, String kodeBuku){
+        this.judul = judul;
+        this.kodeBuku = kodeBuku;
+        this.daftarGenre = new ArrayList<>();
+        this.peminjam = null;
+    }
+
+    public String getJudul(){
+        return judul;
+    }
+
+    public String getKodeBuku(){
+        return kodeBuku;
+    }
+
+    public void tambahGenre(GenreBuku g){
+        if(!daftarGenre.contains(g)){
+            daftarGenre.add(g);
+            g.tambahBuku(this); // sinkronisasi dua arah
+        }
+    }
+
+    public Member getPeminjam(){
+        return peminjam;
+    }
+
+    public boolean isTersedia(){
+        return peminjam == null;
+    }
 }
