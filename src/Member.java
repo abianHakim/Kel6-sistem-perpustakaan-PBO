95b4f812ae4253fdc859c8719cbd65e802990909
nszdaa <fida.ziyanisa@gmail.com>
2026-09-06 21:55:08 +0700

    update member

--- src/Member.java
+++ src/Member.java
@@ -1,3 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Member {
-    
+    private String nama;
+    private String idMember;
+    private List<Buku> daftarPinjaman; // one-to-many ke Bukugi
+
+    public Member(String nama, String idMember){
+        this.nama = nama;
+        this.idMember = idMember;
+        this.daftarPinjaman = new ArrayList<>();
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public String getIdMember(){
+        return idMember;
+    }
 }
